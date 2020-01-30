ae7685b82f2e924bbb9c64c6b01989dbe23ca371
Oketayot Julius peter <jpetexi@gmail.com>
2020-01-30 08:25:55 +0300

    Updates

---
+++ precache-manifest.d1c184ca68c543d7bdd6998d2793d57b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a672bdd23ae542aa304f0f0e3a5e53",
+    "url": "/employees-scoreboar/index.html"
+  },
+  {
+    "revision": "9ebd2c0268160e77345e",
+    "url": "/employees-scoreboar/static/css/main.bc97b8c8.chunk.css"
+  },
+  {
+    "revision": "6469cb084ca1ae122706",
+    "url": "/employees-scoreboar/static/js/2.07686449.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/employees-scoreboar/static/js/2.07686449.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ebd2c0268160e77345e",
+    "url": "/employees-scoreboar/static/js/main.460f709e.chunk.js"
+  },
+  {
+    "revision": "157f79aee7ab521508b9",
+    "url": "/employees-scoreboar/static/js/runtime-main.1e43337a.js"
+  }
+]);
